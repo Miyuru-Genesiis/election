cf9db27e64af9d30c3eb7bc3c8493389f6630e47
Jayani Hellarawa <Jayani@JWT-28-108.trav.jw>
2019-11-14 16:17:31 +0530

    master - edited to modularize plotting functions

--- Scripts/data_analysis.R
+++ Scripts/data_analysis.R
@@ -2,13 +2,17 @@
 # set working directory
 setwd("E:/Studies/R/election-survey/Scripts")
 
-library(datasets)
-library(ggplot2)
-library(plyr)
+#library(ggplot2)
+#library(plyr)
+
+source("plotting.R")
 
 
 if (!require("pacman")) install.packages("pacman")
 pacman::p_load(pacman, rio) 
+
+if (!require("ggrepel")) install.packages("ggrepel")
+
 
 # read file
 readFile <- function(fileName){
@@ -23,140 +27,73 @@ readFile <- function(fileName){
   
 }
 
-# get the frequency of categorical data
-# get frequncies for each category
-# summarises for a given column value
-countColumnFreq <- function (df, columneNumber){
-  y <- count(df[columneNumber])
-  return(y)
-}
-
-# creates a dataFrame
-createDataFrame <- function(labels, values){
-  myFrame <- data.frame(
-    vote_summary = labels,
-    freq = values
-  )
-  return(myFrame)
-}
-
-setPlotName <- function(plotName){
-  png(file = plotName)
-}
-
-# plot the figure - pie chart
-plotFigure <- function(barChart){
-
-  pie <- barChart + coord_polar("y", start=0)
-  pie
-}
 
 
 #### process initial dataset
-#load the dataset
+# load the dataset
 df <- readFile("data.csv")
 
 # process issue summary question
 y <- countColumnFreq(df, 6)
-y
+
+# create dataframe
 dataFrame <- createDataFrame(y$vote_summary, y$freq)
-dataFrame
 
+# plotting
 setPlotName('../Figures/issue_summery.png')
-
-# plot of vote summary question
-bp<- ggplot(myFrame, aes(x="", y=freq, fill=vote_summary))+
-  geom_bar(width = 1, stat = "identity")
-
-
-plotFigure(bp)
+createDonutChart(dataFrame)
 dev.off()
+
 
 
 # process national security question
 y <- countColumnFreq(df, 15)
-y
 dataFrame2 <- createDataFrame(y$national_security, y$freq)
 
-dataFrame2
 setPlotName('../Figures/national_security.png')
-
-# plot of national security data
-bp2<- ggplot(dataFrame2, aes(x="", y=freq, fill=vote_summary))+
-  geom_bar(width = 1, stat = "identity")
-
-bp2
-plotFigure(bp2)
-
+createDonutChart(dataFrame2)
 dev.off()
+
+
 
 # process national development question
 y <- countColumnFreq(df, 16)
-y
 dataFrame3 <- createDataFrame(y$national_development, y$freq)
-
-dataFrame3
-setPlotName('../Figures/national_development.png')
-
-# plot of national security data
-bp3<- ggplot(dataFrame3, aes(x="", y=freq, fill=vote_summary))+
-  geom_bar(width = 1, stat = "identity")
-
-bp3
-plotFigure(bp3)
+setPlotName('../Figures/national_development_d.png')
+createDonutChart(dataFrame3)
 
 dev.off()
+
+
 
 # process unemployemnt question
 y <- countColumnFreq(df, 17)
-y
 dataFrame4 <- createDataFrame(y$unemployemnt, y$freq)
 
-dataFrame4
 setPlotName('../Figures/unemployemnt.png')
-
-# plot of unemployemnt data
-bp4<- ggplot(dataFrame4, aes(x="", y=freq, fill=vote_summary))+
-  geom_bar(width = 1, stat = "identity")
-
-
-plotFigure(bp4)
+createDonutChart(dataFrame4)
 
 dev.off()
+
 
 
 # process cost_of_living question
 y <- countColumnFreq(df, 18)
-y
 dataFrame5 <- createDataFrame(y$cost_of_living, y$freq)
 
-dataFrame5
 setPlotName('../Figures/cost_of_living.png')
-
-# plot of cost_of_living data
-bp5 <- ggplot(dataFrame5, aes(x="", y=freq, fill=vote_summary))+
-  geom_bar(width = 1, stat = "identity")
-
-
-plotFigure(bp5)
+createDonutChart(dataFrame5)
 
 dev.off()
 
 
+
 # process religious_ethnic_issues question
 y <- countColumnFreq(df, 19)
-y
 dataFrame6 <- createDataFrame(y$religious_ethnic_issues, y$freq)
 
-dataFrame6
 setPlotName('../Figures/religious_ethnic_issues.png')
-
-# plot of religious_ethnic_issues data
-bp6 <- ggplot(dataFrame5, aes(x="", y=freq, fill=vote_summary))+
-  geom_bar(width = 1, stat = "identity")
-
-
-plotFigure(bp6)
+createDonutChart(dataFrame6)
 
 dev.off()
 
